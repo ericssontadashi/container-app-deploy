0c03c2a3463d27225b3abedaef6e869f134877c1
Ericsson de Oliveira <cssonddos@gmail.com>
2024-03-29 14:57:11 -0300

    changed host listen addr

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(dateTime);
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
